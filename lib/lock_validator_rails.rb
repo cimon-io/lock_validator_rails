80da49e807163d9ed79f660fd09d820193b9c459
K-S-A <atillas@ukr.net>
2017-02-24 03:19:58 +0200

    Update LockValidator#validate_each

--- lib/lock_validator_rails.rb
+++ lib/lock_validator_rails.rb
@@ -3,8 +3,18 @@ require 'active_record'
 
 module LockValidatorRails
   class LockValidator < ActiveModel::EachValidator
+
+    def initialize(options)
+      options[:message] ||= :is_outdated
+      options[:with] ||= :updated_at
+      options[:normalizer] ||= :to_microseconds
+      super
+    end
+
     def validate_each(record, attribute, value)
-      record.errors[attribute] << (options[:message] || 'wrong email address')
+      return if record.public_send(options[:normalizer], options[:with]) == value
+      error_key = options.fetch(:error_key, attribute)
+      record.errors.add(error_key, :outdated, message: options[:message])
     end
   end
 end
